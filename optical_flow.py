6f3a4f281c23beca4714da81a0fe8a6c01d0252e
Zihao He <awayhosi@hotmail.com>
2020-06-15 18:21:41 +0200

    calculating OF with features from former frame instead of current frame.

--- optical_flow.py
+++ optical_flow.py
@@ -4,6 +4,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# choose a feature:
+FEATURE = 'Harris'	# 'Harris' or 'SIFT'
+OUTPUT_TO_FILE = True
+OUTPUT_FILE_NAME = '../../Videos/OF_trace_with_'+FEATURE+'.avi'
+
 # calculation of euclidian distance
 def eucl_dist(m, n, o, p):
 	return np.sqrt(m**2 + n**2 + o**2 + p**2)
@@ -20,38 +25,36 @@ lk_params = dict(winSize=(15,15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | 
 # open object file
 file = cv2.VideoCapture('../../Videos/output.avi')
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('../../Videos/OF_trace_with_SIFT.avi', fourcc, 40.0, (450,800))
+if(OUTPUT_TO_FILE): out = cv2.VideoWriter(OUTPUT_FILE_NAME, fourcc, 40.0, (450,800))
 
 # SIFT feature detector
-sift = cv2.xfeatures2d.SIFT_create()
+if(FEATURE=='SIFT'):	sift = cv2.xfeatures2d.SIFT_create()
 
 while(file.isOpened()):
 	ret, frame = file.read()
 	if(ret):
 		frame_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		# do feature extraction
-		# 1) with Harris corner detector
-		points = cv2.goodFeaturesToTrack(frame_g, maxCorners, qualityLvl, minDist)
-		# 2) with SIFT feature detector
-		key_points, des = sift.detectAndCompute(frame_g, None)
-		key_points_for_LK = np.array([])
-		for kp in key_points:
-			key_points_for_LK = np.append(key_points_for_LK, [kp.pt[0],kp.pt[1]])
-		key_points_for_LK = np.float32(key_points_for_LK.reshape(int(len(key_points_for_LK)/2),1,2))
+		if(FEATURE=='Harris'):
+			# 1) with Harris corner detector
+			points = cv2.goodFeaturesToTrack(frame_g, maxCorners, qualityLvl, minDist)
+		elif(FEATURE=='SIFT'):
+			# 2) with SIFT feature detector
+			key_points, des = sift.detectAndCompute(frame_g, None)
+			# reshape results of SIFT detector for LK method calculation
+			points = np.array([])
+			for kp in key_points:
+				points = np.append(points, [kp.pt[0],kp.pt[1]])
+			points = np.float32(points.reshape(int(len(points)/2),1,2))
 		if(len(former_frame)):
-			# calculate Optical Flow with:
-			# 1) Harris corners
-			# p_op_flow, status, error = cv2.calcOpticalFlowPyrLK(former_frame, frame_g, points, None, **lk_params)
-			# 2) SIFT feature
-			p_op_flow, status, error = cv2.calcOpticalFlowPyrLK(former_frame, frame_g, key_points_for_LK, None, **lk_params)
-
-			# obviously all the errors are not equal/close, cause there is spatial information missing
-			# if we have another channel of 'depth', we can reconstruct the spatial relationship and estimate
+			# calculate Optical Flow with points we extracted:
+			p_op_flow, status, error = cv2.calcOpticalFlowPyrLK(former_frame, frame_g, former_points, None, **lk_params)
+			# obviously all the errors are not equal/close, cause there is spatial information missing if  
+			# we have another channel of 'depth', we can reconstruct the spatial relationship and estimate
 			# the 3D motion of camera(if motion_obj known)/object(if motion_obj known)
 			# print('Error between frames: \n'+str(error))
 			relevant_pts = p_op_flow[status==1]
-			#former_relevant_pts = points[status==1]
-			former_relevant_pts = key_points_for_LK[status==1]
+			former_relevant_pts = former_points[status==1]
 			it = 0
 			for i, (rel_pt, form_rel_pt) in enumerate(zip(relevant_pts, former_relevant_pts)):
 				x, y = rel_pt.ravel()
@@ -71,17 +74,19 @@ while(file.isOpened()):
 		for point in points:
 			cv2.circle(frame, (point[0][0], point[0][1]), 3, (0,0,255), 1)	# Harris features
 		'''
-		cv2.drawKeypoints(frame,key_points,frame)							# SIFT features
+		#cv2.drawKeypoints(frame,key_points,frame)							# SIFT features
 		if(len(former_frame)):												# optical flow trace
 			for i, (pt, f_pt) in enumerate(zip(relevant_pts, former_relevant_pts)):
+				cv2.circle(frame, (pt[0], pt[1]), 3, (0,0,255), 1)
 				cv2.line(frame, (pt[0],pt[1]), (f_pt[0],f_pt[1]), (0, 255, 0), 1)
 		cv2.imshow('Motion', frame)
-		#out.write(frame)
+		if(OUTPUT_TO_FILE): out.write(frame)
 		former_frame = frame_g
+		former_points = points
 	else:
 		break
 	if(cv2.waitKey(25) &0xFF == ord('q')):
 		break
 file.release()
-#out.release()
+if(OUTPUT_TO_FILE): out.release()
 cv2.destroyAllWindows()
